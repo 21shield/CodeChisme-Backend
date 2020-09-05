4b49145943d48e4042c9a9a94abb2b4de6be7cc8
21shield <21netaly@gmail.com>
2020-09-05 19:06:44 -0400

    added somegems

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :chatrooms
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
 end
