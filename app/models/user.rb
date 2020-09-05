0605e958a48ff23b4929a83cd4d6148477a8768e
21shield <21netaly@gmail.com>
2020-09-05 17:35:52 -0400

    added validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
-
     has_many :messages
     has_many :chatrooms, through: :messages
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :username, uniqueness: { case_sensitive: false }
+
+
 
 end
